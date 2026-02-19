076660f29ed7bb4623e038c01a116ae7db541433
wirnty <skedovichusjdj@gmail.com>
2026-02-19 11:24:55 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="expections",
+    version="0.2.7",  # increment this for new releases
+    py_modules=["expections"],  # single-file module
+    python_requires=">=3.9",
+    description="A simple module that can make custom expections in specific commands.",
+    author="wirnty",
+    author_email="skedovichusjdj@gmail.com",
+    url="https://pypi.org/project/expections/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
